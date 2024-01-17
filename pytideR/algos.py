586bcc3e85b5c59cf477d3531232324ad55de744
Raphael Dussin <Raphael.Dussin@noaa.gov>
2024-01-17 13:03:50 -0500

    catching up

--- pytideR/algos.py
+++ pytideR/algos.py
@@ -8,11 +8,12 @@ def reflect_angle_id(angle_incident, angle_wall, NAngle=24):
     NAngle_d2 = (NAngle / 2)
 
     angle_relatif = np.mod(angle_incident -angle_wall + NAngle, NAngle)
-    if (0 < np.mod(angle_incident - angle_wall + NAngle, NAngle) < NAngle_d2):
+    if (0 < angle_relatif < NAngle_d2):
         a_r = 2 * angle_wall - angle_incident
         a_r = np.mod(a_r + NAngle, NAngle)
     else:
         a_r = None
+        raise ValueError("Ray is not incident")
     return a_r
 
 
